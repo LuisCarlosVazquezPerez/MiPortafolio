84c32f9084d8d1438399e3a6215e57798d1b1d90
LuisVazquezPerez <LuisCarlosVazquezPerez@gmail.com>
2023-11-16 20:47:20 -0600

    EMPEZAR CON LAYOUT

--- resources/js/app.js
+++ resources/js/app.js
@@ -16,14 +16,3 @@ import {
 
 initTE({ Input, Select, Collapse   });
 
-
-// import Dropzone from 'dropzone';
-// Dropzone.autoDiscover = false;
-// const dropzone = new Dropzone('#dropzone', {
-//   dictDefaultMessage: "Sube aqui tu imagen",
-//   acceptedFiles: ".png, .jpg, jpeg, .gif",
-//   addRemoveLinks: true,
-//   dictRemoveFile: 'Borrar Archivo',
-//   maxFiles: 1,
-//   uploadMultiple: false,
-// });
--- resources/views/luiscvp.blade.php
+++ resources/views/luiscvp.blade.php
@@ -1,9 +1,4 @@
 <x-app-layout>
     @include('layouts.navegacion')
-    <h1 class="bg-red-200">Hola</h1>
-
-
-    <a href="{{route('proyectos.index')}}">Proyectos</a>
-    <a href="{{route('reconocimientos.index')}}">Reco</a>
 
 </x-app-layout>
